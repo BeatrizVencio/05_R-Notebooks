4189115607d7e24007ad7d1a82102a56ba89793a
Beatriz Vencio Moreira Nascimento <165849751+BeatrizVencio@users.noreply.github.com>
2024-05-21 16:30:53 -0300

    Update 03 - Data_Frames_e_Vetores.R

--- 03 - Data_Frames_e_Vetores.R
+++ 03 - Data_Frames_e_Vetores.R
@@ -125,3 +125,75 @@ seq(from = 1, to = 10, by = 2)
 
 x <- seq(0, 100, length.out = 5)
 print(x)
+
+#################### ATIVIDADE AVALIATIVA 2 #################### 
+# PARTE 1
+# 1- Um data frame é uma estrutura de dados usada para armazenar 
+# dados tabulados, onde cada coluna pode conter elementos de 
+# diferentes tipos, ao contrário da matriz que só armazena um 
+# tipo de dado
+
+# 2- Operadores básicos: +, -, *, /, ^, %%, %/%, sqrt()
+a <- 5
+b <- 3
+soma <- a + b
+subtracao <- a - b
+multiplicacao <- a * b
+
+a <- 10
+b <- 2
+divisao <- a / b
+
+a <- 2
+b <- 3
+exponenciacao <- a^b
+
+a <- 10
+b <- 3
+resto <- a %% b
+divisao_inteira <- a %/% b
+
+a <- 9
+raiz_quadrada <- sqrt(a)
+print(raiz_quadrada)
+
+# 3- Operadores Relacionais: >, <, >=, <=, ==, != para comparar valores
+#    Operadores Lógicos: & (AND), | (OR), ! (NOT) para combinar condições
+
+5 > 3 # TRUE
+5 < 3 # FALSE
+5 >= 3 # TRUE
+5 <= 3 # FALSE
+10 == 10 # TRUE
+10 != 10 # FALSE
+
+(5 > 3) & (10 <= 20) # TRUE
+(5 > 3) | (10 == 20) # TRUE
+!(5 == 5) # FALSE
+
+# PARTE 2
+estudantes <- data.frame(
+  nome = c("Ana", "Bruno", "Carlos", "Diana", "Eduardo"),
+  idade = c(20, 22, 23, 19, 25),
+  curso = c("Ciência de Dados", "Engenharia", "Matemática", "Ciência da Computação", "Estatística"),
+  nota_final = c(8.5, 5.0, 7.0, 9.0, 6.5)
+)
+print(estudantes)
+
+nota_maior <- estudantes[estudantes$nota_final >= 7, ]
+print(nota_maior)
+
+idade_maior <- estudantes[estudantes$idade < 25 & estudantes$nota_final >= 7, ] 
+print(idade_maior)
+
+# PARTE 3
+media_nota_final <- mean(estudantes$nota_final)
+print(media_nota_final)
+
+estudantes$aprovado <- estudantes$nota_final >= 7
+print(estudantes)
+
+total_estudantes <- nrow(estudantes)
+aprovados <- sum(estudantes$aprovado == TRUE)
+percent_aprovados <- (aprovados / total_estudantes) * 100
+print(percent_aprovados )
